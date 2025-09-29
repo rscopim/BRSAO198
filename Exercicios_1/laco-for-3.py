4b6ca1c48e969a35e0f545f27c39eade11f7f23b
rscopim <ricardo.simines@gmail.com>
2025-09-29 19:10:13 -0300

    Atualização

---
+++ Exercicios_1/laco-for-3.py
@@ -0,0 +1,9 @@
+'''3 - Crie um programa em Python que utilize um laço for para exibir os números de 1 a 10 na tela. '''
+
+for i in range(1, 11):
+    print(i)
+
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
